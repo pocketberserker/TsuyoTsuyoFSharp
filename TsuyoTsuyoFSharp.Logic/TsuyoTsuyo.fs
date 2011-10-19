aabd97bcb864c68cbe3e40fe3c233aa2235c69b7
pocketberserker <pocketberserker@gmail.com>
2011-10-19 20:16:48 +0900

    障害物が存在する場合のつよオブジェクトの右回転を実装

--- TsuyoTsuyoFSharp.Logic/TsuyoTsuyo.fs
+++ TsuyoTsuyoFSharp.Logic/TsuyoTsuyo.fs
@@ -27,8 +27,8 @@ type TsuyoObj (status1:TwitterStatus option, status2:TwitterStatus option) =
 
   let createTsuyo status pos hidden =
     match status with
-      | Some x -> new Tsuyo(pos, Real x, hidden)
-      | None -> new Tsuyo(pos, Dummy, hidden)
+    | Some x -> new Tsuyo(pos, Real x, hidden)
+    | None -> new Tsuyo(pos, Dummy, hidden)
 
   let tsuyo1 = createTsuyo status1 (RowNum+RowNum/2-1) false
   
@@ -38,18 +38,64 @@ type TsuyoObj (status1:TwitterStatus option, status2:TwitterStatus option) =
 
   member x.Tsuyo2 with get() = tsuyo2 and set(t) = tsuyo2 <- t
 
-let rotateR (tobj:TsuyoObj) = 
-  match tobj.Tsuyo2 with
-    | (tsuyo, SndTsuyoPos.Right) ->
-      tsuyo.Pos <- tobj.Tsuyo1.Pos + RowNum
-      tobj.Tsuyo2 <- tsuyo, SndTsuyoPos.Down
-    | (tsuyo, SndTsuyoPos.Down) ->
-      tsuyo.Pos <- tobj.Tsuyo1.Pos - 1
-      tobj.Tsuyo2 <- tsuyo, SndTsuyoPos.Left
-    | (tsuyo, SndTsuyoPos.Left) ->
-      tsuyo.Pos <- tobj.Tsuyo1.Pos - RowNum
-      tobj.Tsuyo2 <- tsuyo, SndTsuyoPos.Up
-    | (tsuyo, SndTsuyoPos.Up) ->
-      tsuyo.Pos <- tobj.Tsuyo1.Pos + 1
-      tobj.Tsuyo2 <- tsuyo, SndTsuyoPos.Right
+let mutable fieldTsuyo:Tsuyo list = []
 
+let avoidance (tsuyo1:Tsuyo) (tsuyo2:Tsuyo) (pos:SndTsuyoPos) =
+
+  let collide' f = List.exists f fieldTsuyo
+
+  let (|LeftCollide|RightCollide|Other|) =
+    function
+    | SndTsuyoPos.Up ->
+      match tsuyo1.Pos % RowNum = RowNum - 1 with 
+      | true ->
+        match collide' (fun (x:Tsuyo) -> x.Pos = tsuyo1.Pos - 1) with
+        | true -> RightCollide true
+        | false -> RightCollide false
+      | false ->
+        match collide' (fun (x:Tsuyo) -> x.Pos = tsuyo1.Pos + 1) with
+        | true -> RightCollide true
+        | false -> Other
+    | SndTsuyoPos.Down ->
+      match tsuyo1.Pos % RowNum = 0 with
+      | true ->
+        match collide' (fun (x:Tsuyo) -> x.Pos = tsuyo2.Pos + 1) with
+        | true -> LeftCollide true
+        | false -> LeftCollide false
+      | false ->
+        match collide' (fun (x:Tsuyo) -> x.Pos = tsuyo2.Pos - 1) with
+        | true -> LeftCollide true
+        | false -> Other
+    | _ -> Other
+  
+  match pos with
+  | RightCollide false ->
+    tsuyo1.Pos <- tsuyo1.Pos - 1
+    tsuyo2.Pos <- tsuyo2.Pos - 1
+    true
+  | RightCollide true -> false
+  | LeftCollide false ->
+    tsuyo1.Pos <- tsuyo2.Pos + 1
+    true
+  | LeftCollide true -> false
+  | Other -> true
+
+let rotateR (tobj:TsuyoObj) =
+
+  let rotate' (tsuyo1:Tsuyo) (tsuyo2:Tsuyo) f (num:int) (pos:SndTsuyoPos) =
+    tsuyo2.Pos <- f tsuyo1.Pos num
+    tsuyo2, pos
+
+  match tobj.Tsuyo2 with
+  | (tsuyo2, SndTsuyoPos.Right) ->
+    tobj.Tsuyo2 <- rotate' tobj.Tsuyo1 tsuyo2 (+) RowNum SndTsuyoPos.Down
+  | (tsuyo2, SndTsuyoPos.Left) ->
+    tobj.Tsuyo2 <- rotate' tobj.Tsuyo1 tsuyo2 (-) RowNum SndTsuyoPos.Up
+  | (tsuyo2, pos) ->
+    match avoidance tobj.Tsuyo1 tsuyo2 pos with
+    | true ->
+      match pos with
+      | SndTsuyoPos.Up -> tobj.Tsuyo2 <- rotate' tobj.Tsuyo1 tsuyo2 (+) 1 SndTsuyoPos.Right
+      | SndTsuyoPos.Down -> tobj.Tsuyo2 <- rotate' tobj.Tsuyo1 tsuyo2 (-) 1 SndTsuyoPos.Left
+      | _ -> ()
+    | false -> ()
--- TsuyoTsuyoFSharp.Scenario/TsuyoTsuyoScenario.fs
+++ TsuyoTsuyoFSharp.Scenario/TsuyoTsuyoScenario.fs
@@ -40,7 +40,7 @@ module 周りに障害物がない =
       
     [<Scenario>]
     let ``第2つよが下にある時の右回転はつよが左に移動する``() =
-      let tobj = changePos (RowNum+RowNum/2-2) SndTsuyoPos.Down
+      let tobj = changePos (RowNum*2+2) SndTsuyoPos.Down
     
       Given tobj
       |> When rotateR
@@ -62,3 +62,102 @@ module 周りに障害物がない =
       |> It should equal (RowNum/2-1)
       |> Verify
   
+module 障害物がある =
+
+  module 第2つよを右回転 =
+
+    [<Scenario>]
+    let ``右壁隣接時の右回転は第1つよが左に、第2つよが第1つよの右に移動する``() =
+      let tobj = changePos (RowNum-1) SndTsuyoPos.Up
+      tobj.Tsuyo1.Pos <- RowNum*2-1
+      TsuyoTsuyo.fieldTsuyo <- []
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum*2-2)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Right
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum*2-1)
+      |> Verify
+
+    [<Scenario>]
+    let ``第1つよの右側が壁で左側につよが存在する場合、回転できずにそのままの位置を保つ``() =
+      let tobj = changePos (RowNum-1) SndTsuyoPos.Up
+      tobj.Tsuyo1.Pos <- RowNum*2-1
+      TsuyoTsuyo.fieldTsuyo <- [new Tsuyo(RowNum*2-2,TsuyoType.Dummy,false)]
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum*2-1)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Up
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum-1)
+      |> Verify
+
+    [<Scenario>]
+    let ``第2つよの左側につよが存在する場合、回転できずにそのままの位置を保つ``() =
+      let tobj = changePos (RowNum*3-1) SndTsuyoPos.Down
+      tobj.Tsuyo1.Pos <- RowNum*2-1
+      TsuyoTsuyo.fieldTsuyo <- [new Tsuyo(RowNum*3-2,TsuyoType.Dummy,false)]
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum*2-1)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Down
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum*3-1)
+      |> Verify
+
+    [<Scenario>]
+    let ``左壁隣接時の右回転は第1つよが第2つよの右に移動し、第2つよはそのまま``() =
+      let tobj = changePos (RowNum*2) SndTsuyoPos.Down
+      tobj.Tsuyo1.Pos <- RowNum
+      TsuyoTsuyo.fieldTsuyo <- []
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum*2+1)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Left
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum*2)
+      |> Verify
+
+    [<Scenario>]
+    let ``第2つよの左側が壁で右側につよが存在する場合、回転できずにそのままの位置を保つ``() =
+      let tobj = changePos (RowNum*2) SndTsuyoPos.Down
+      tobj.Tsuyo1.Pos <- RowNum
+      TsuyoTsuyo.fieldTsuyo <- [new Tsuyo(RowNum*2+1,TsuyoType.Dummy,false)]
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Down
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum*2)
+      |> Verify
+
+    [<Scenario>]
+    let ``第1つよの右側につよが存在する場合、回転できずにそのままの位置を保つ``() =
+      let tobj = changePos (RowNum-2) SndTsuyoPos.Up
+      tobj.Tsuyo1.Pos <- RowNum*2-2
+      TsuyoTsuyo.fieldTsuyo <- [new Tsuyo(RowNum*2-1,TsuyoType.Dummy,false)]
+
+      Given tobj
+      |> When rotateR
+      |> Whereas tobj.Tsuyo1.Pos
+      |> It should equal (RowNum*2-2)
+      |> Whereas (snd tobj.Tsuyo2)
+      |> It should equal SndTsuyoPos.Up
+      |> Whereas (fst tobj.Tsuyo2).Pos
+      |> It should equal (RowNum-2)
+      |> Verify
