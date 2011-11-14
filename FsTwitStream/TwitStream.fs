2c5ca58d2ad60b5302729f5181227ebf82d720be
pocketberserker <pocketberserker@gmail.com>
2011-11-15 03:58:44 +0900

    Twitterizerのバージョンを更新
    2.3.3を2.4.0に変更

--- FsTwitStream/TwitStream.fs
+++ FsTwitStream/TwitStream.fs
@@ -22,29 +22,6 @@ let settingToken (token:OAuthTokens)  =
   token.set_AccessToken(tokenData.[3])
   token.set_AccessTokenSecret(tokenData.[4])
 
-// twitterizer内のURLが最新のStreaming APIに対応していないためstartPublicStreamを自作
-let startMyPublicStream (stream:TwitterStream) stopped created =
-  
-  let setFieldData name data =
-    let field = stream.GetType().GetField(name, BindingFlags.Instance ||| BindingFlags.NonPublic)
-    field.SetValue(stream, data)
-
-  let builder = new WebRequestBuilder(new Uri("https://stream.twitter.com/1/statuses/filter.json"), HTTPVerb.POST, stream.Tokens, true, userAgent)
-  
-  let prepareStreamOptions = stream.GetType().GetMethod("PrepareStreamOptions", BindingFlags.Instance ||| BindingFlags.NonPublic)
-  prepareStreamOptions.Invoke(stream, [|builder|]) |> ignore
-  
-  let request = builder.PrepareRequest()
-
-  setFieldData "streamStoppedCallback" stopped
-  setFieldData "statusCreatedCallback" created
-  setFieldData "stopReceived" false
-
-  let StreamCallback = stream.GetType().GetMethod("StreamCallback", BindingFlags.Instance ||| BindingFlags.NonPublic)  
-  let callback = new AsyncCallback (fun result -> StreamCallback.Invoke(stream, [|result|]) |> ignore)
-  
-  request.BeginGetResponse(callback, request) 
-
 let startStream token streamMode track created =
 
   settingToken token
@@ -60,7 +37,7 @@ let startStream token streamMode track created =
 
   while true do ()
 
-let publicStream stream stopped created = startMyPublicStream stream stopped created
+let publicStream (stream:TwitterStream) stopped created = stream.StartPublicStream(stopped, created, null, null, null)
 
 let userStream (stream:TwitterStream) stopped created = stream.StartUserStream(null, stopped, created, null, null, null, null)
 
